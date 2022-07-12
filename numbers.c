59f9c7c97f8785cda2673fc5be6dd3e1495e9569
Maranga <rchdmaranga@gmail.com>
2022-07-13 00:31:17 +0300

    file

--- numbers.c
+++ numbers.c
@@ -1,16 +1,75 @@
 #include "main.h"
-
 /**
- * print_binary - function that prints the binary representation of a number
- * @n: number to be printed in binary
- * @printed: hold the number of characters printed
+ * print_i - prints an integer
+ * @i: integer to print
+ *
+ * Return: number of chars and digits printed
  */
-void print_binary(unsigned int n, unsigned int *printed)
+int print_i(va_list i)
 {
-	if (n > 1)
+	int a[10];
+	int j, m, n, sum, count;
+	n = va_arg(i, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
 	{
-		*printed += 1;
-		print_binary(n >> 1, printed);
+		m /= 10;
+		a[j] = (n / m) % 10;
 	}
-	_putchar((n & 1) + '0');
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
+}
+/**
+ * print_d - prints a decimal
+ * @d: decimal to print
+ *
+ * Return: number of chars and digits printed
+ */
+int print_d(va_list d)
+{
+	int a[10];
+	int j, m, n, sum, count;
+	n = va_arg(d, int);
+	count = 0;
+	m = 1000000000;
+	a[0] = n / m;
+	for (j = 1; j < 10; j++)
+	{
+		m /= 10;
+		a[j] = (n / m) % 10;
+	}
+	if (n < 0)
+	{
+		_putchar('-');
+		count++;
+		for (j = 0; j < 10; j++)
+			a[j] *= -1;
+	}
+	for (j = 0, sum = 0; j < 10; j++)
+	{
+		sum += a[j];
+		if (sum != 0 || j == 9)
+		{
+			_putchar('0' + a[j]);
+			count++;
+		}
+	}
+	return (count);
 }
